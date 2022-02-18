fe589f74c9a36385eba54965badccc1ab849368e
Budee0117 <91043221+Budee0117@users.noreply.github.com>
2022-02-18 11:03:40 +0800

    Create cin.cpp

---
+++ week_1/cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "what is your name \n";
+	cin >> name;
+	cout << "Hello " << name;
+	string hobby;
+	cout << " what is your hobby\n";
+	cin >> hobby;
+	cout << "playing " << hobby;
+
+	return 0;
+}
