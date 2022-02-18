a21e71a4cad6c1e5d517d5f0f4a1c049a53513f6
Budee0117 <91043221+Budee0117@users.noreply.github.com>
2022-02-18 10:49:40 +0800

    Create types.cpp

---
+++ week_1/types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	cout << "Hello World\n\a";
+
+	int a = 7;
+	
+	//Double
+	double c = { 5.4 } ;
+	
+	//String
+	string e = "Hello" ;
+	
+	//boolean
+	bool g = true;
+	cout << g;
+	cout << a << "\n" << c << "\n" << e
+		<< "\n" << g << "\n";
+
+	return 0;
+}
