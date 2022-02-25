651163394d16ce1c710d747ccd7c808b2bc2c2d9
Budee0117 <91043221+Budee0117@users.noreply.github.com>
2022-02-25 10:43:36 +0800

    Create enum_switch.cpp

---
+++ week_2/enum_switch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	enum Name
+	{
+		michealJordan,
+		lebronJames,
+		kobeBryant
+};
+	Name nameEnum = lebronJames;
+	switch (nameEnum)
+	{
+	case michealJordan: 
+		cout << "5star";
+		break;
+	case lebronJames:
+		cout << "4star";
+		break;
+	case kobeBryant:
+		cout<< "3star";
+		break;
+	default:
+		cout << "other basketball players 1-2 star";
+		break;
+	}
+}
