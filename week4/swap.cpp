4a4e83b1457c5bf08c42d79dc3966fdba5bbb697
Budee0117 <91043221+Budee0117@users.noreply.github.com>
2022-03-11 10:18:37 +0800

    Create swap.cpp

---
+++ week4/swap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+void swap(int &a, int &b);
+int main()
+{
+    int a=2;
+    int b=4;
+    std::cout<<"a="<<a<<"\n";
+    std::cout<<"b="<<b<<"\n";
+    swap(a,b);
+    std::cout<<"a="<<a<<"\n";
+    std::cout<<"b="<<b<<"\n";
+
+
+    return 0;
+}
+void swap (int &a, int &b)
+{
+    int c=a;
+     a=b;
+     b=c;
+}   
